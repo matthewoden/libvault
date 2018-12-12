a6ee22a15df47c06fec85ec08c6223822234f289
matthewoden <heymatthewoden@gmail.com>
2018-12-11 21:32:58 -0600

    bump version 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vault.MixProject do
   def project do
     [
       app: :libvault,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
