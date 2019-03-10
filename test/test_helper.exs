fb88905befaef18ea66b83c2688e4d65468a888e
Philip Sampaio Silva <philip.sampaio@gmail.com>
2019-03-10 13:37:35 -0300

    Run tests with a deterministic Tesla adapter
    It avoids failing without knowing which of the Tesla adapter was
    responsible or related to the failure.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,15 @@
-ExUnit.start()
 Application.ensure_all_started(:bypass)
 Application.ensure_all_started(:hackney)
 Application.ensure_all_started(:ibrowse)
-Application.put_env(:tesla, :adapter, Enum.random([Tesla.Adapter.Hackney, Tesla.Adapter.Ibrowse]))
+
+tesla_adapter =
+  case System.get_env("TESLA_ADAPTER") do
+    "ibrowser" ->
+      Tesla.Adapter.Ibrowse
+    _ ->
+      Tesla.Adapter.Hackney
+  end
+Application.put_env(:tesla, :adapter, tesla_adapter)
 Application.ensure_all_started(:tesla)
+
+ExUnit.start()
