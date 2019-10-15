9019bc984c8fa52bd2a76ba3c4e765e4038bfc0b
matthewoden <heymatthewoden@gmail.com>
2019-10-15 10:57:10 -0500

    patch 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vault.MixProject do
   def project do
     [
       app: :libvault,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
